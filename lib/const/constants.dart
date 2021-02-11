7476b20dff01c8f893a12157c2700a1a0e12f823
Purush <purushottam.yadav@yellowmessenger.com>
2021-02-11 11:42:06 +0530

    Updated chatbot url

--- lib/const/constants.dart
+++ lib/const/constants.dart
@@ -1,3 +1,5 @@
 // Fetch and start the chatbot webview
-final String BASE_URL =
+const String BASE_URL_GITHUB =
     "https://yellowmessenger.github.io/pages/app/mobile.html";
+const String BASE_URL =
+    "https://app.yellowmessenger.com/components/public/webviews/mobile-sdk/index.html";
